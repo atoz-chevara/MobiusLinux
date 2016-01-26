15d7cb41667576fd6739a44a7530d88c35f1d08b
Silent Mobius <michael@linuxsystems.rocks>
2016-01-26 12:03:06 +0200

    adding config files

---
+++ defaults/custom_setup/default.sh
@@ -0,0 +1,5 @@
+apt-get install -y nano vi vim iftop htop openssh-client openssh-server less openssl ca-certificates \
+mate-desktop-environment-core mate-desktop-environment-extra mate-desktop-environment-extras \
+
+echo -e "\033[31mPassword for root\033[0m"
+passwd root
