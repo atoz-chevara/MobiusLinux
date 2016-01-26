f487d0cf0c134b4a2e4f814ce75026c77cb077e5
Silent Mobius <michael@linuxsystems.rocks>
2016-01-26 12:10:27 +0200

    adding config files

--- defaults/custom_setup/default.sh
+++ defaults/custom_setup/default.sh
@@ -1,5 +1,18 @@
 apt-get install -y nano vi vim iftop htop openssh-client openssh-server less openssl ca-certificates \
 mate-desktop-environment-core mate-desktop-environment-extra mate-desktop-environment-extras \
-
+sqlite sqlite3 mysql-client mysql-server postgresql \
+apache2 nginx-full nfs-common samba-common redis-server sysv-rc-conf\
+python-scapy python-pip python-networkx python-netaddr python-netifaces python-netfilter \
+python-gnuplot python-mako python-radix ipython  python-pycurl python-lxml python-libpcap \
+python-nmap python-flask python-scrapy libpoe-component-pcap-perl libnet-pcap-perl perl-modules\
+build-essential python-software-properties debian-goodies freeglut3-dev libxmu-dev libpcap-dev \
+libglib2.0 libxml2-dev libpcap-dev libtool rrdtool autoconf automake autogen \
+wget libsqlite3-dev libhiredis-dev libgeoip-dev debootstrap qemu-user-static \
+device-tree-compiler lzma lzop u-boot-tools pixz dkms git-core gnupg flex bison gperf libesd0-dev \
+zip curl libncurses5-dev zlib1g-dev gcc-multilib g++-multilib libusb-1.0-0 libusb-1.0-0-dev fakeroot\
+firmware-linux firmware-linux-free firmware-linux-nonfree vlc \
+u-boot-tools gparted abiword transmission guake mixxx \
+culmus xfonts-efont-unicode xfonts-efont-unicode-ib xfonts-intl-european \
+msttcorefonts
 echo -e "\033[31mPassword for root\033[0m"
 passwd root
